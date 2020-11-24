d3f2084ef80410a754cc112a2be49501d3c378a0
dilomen <dilomen@outlook.com>
2020-11-24 21:53:20 +0800

    完善

--- bin/index.js
+++ bin/index.js
@@ -5,19 +5,21 @@ const fs = require("fs");
 const execute = require("./execute");
 const ora = require("ora");
 let spinner = ora("serein downloading ...");
-
+const config = require("../package.json");
+const validateProjectName = require('validate-npm-package-name')
 let projectName = "";
 // 尖括号（例如<required>）表示必填参数。方括号（例如[optional]）表示可选参数
 program
-  .version("1.0.0")
+  .version(config.version || "1.0.0")
   .arguments("[name]")
   .action((name) => {
     projectName = name;
   })
   .parse(process.argv);
-if (!projectName) {
-  console.warn("请按以下示例方式安装：\nresein-cli myapp");
-  return false;
+const projectNameVaild = validateProjectName(projectName)
+if (!projectNameVaild) {
+  console.warn("无效的项目名\n请参照指令: resein-cli myapp ");
+  process.exit(1)
 }
 
 template.name = projectName;
